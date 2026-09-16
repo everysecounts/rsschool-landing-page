455550db5f39952d90706f2a6119f6dc83584232
everysecounts <mscintillation@gmail.com>
2026-09-16 22:04:41 +0300

    refactor: del querySelector

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import '@/styles/variables.css';
 import '@/styles/globals.css';
 import '@/styles/fonts.css';
 
-const app = new App(document.querySelector('#app'));
+const app = new App(document.body);
 
 app.start();
