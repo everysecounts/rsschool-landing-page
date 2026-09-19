659b469e2e490eaf49cd32cf73b1ae3bef1e2fcc
everysecounts <mscintillation@gmail.com>
2026-09-19 03:06:57 +0300

    feat: add footer with contacts and social links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Header } from '@/components/Header';
 import { Main } from '@/components/Main';
+import { Footer } from '@/components/Footer';
 import { Router, routes } from '@/router';
 
 export class App {
@@ -7,12 +8,13 @@ export class App {
     this.container = container;
     this.header = new Header();
     this.main = new Main();
+    this.footer = new Footer();
     this.router = new Router(routes, this.main.element, (path) => {
       this.header.setActivePath(path);
     });
   }
   start() {
-    this.container.replaceChildren(this.header.element, this.main.element);
+    this.container.replaceChildren(this.header.element, this.main.element, this.footer.element);
     this.router.start();
   }
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,113 @@
+import { createElement } from '@/utils';
+import { contactIcons, socialIcons } from './icons';
+import styles from './Footer.module.css';
+
+const SOCIAL_LINKS = [
+  { label: 'X', href: 'https://x.com/', icon: socialIcons.twitter },
+  { label: 'Instagram', href: 'https://www.instagram.com/', icon: socialIcons.instagram },
+  { label: 'Facebook', href: 'https://www.facebook.com/', icon: socialIcons.facebook },
+];
+
+const CONTACT_ITEMS = [
+  {
+    label: '8558 Green Rd.,  LA',
+    href: 'https://maps.app.goo.gl/fx5A8m8jbESmfFgJ9',
+    target: '_blank',
+    rel: 'noopener noreferrer',
+    icon: contactIcons.location,
+  },
+  {
+    label: '+1 (603) 555-0123',
+    href: 'tel:+16035550123',
+    icon: contactIcons.phone,
+  },
+  {
+    label: 'Mon–Sat: 9:00–23:00',
+    icon: contactIcons.clock,
+  },
+];
+
+function createSocialLinks() {
+  return SOCIAL_LINKS.map(({ label, href, icon }) =>
+    createElement(
+      'a',
+      {
+        className: styles.socialLink,
+        href,
+        target: '_blank',
+        rel: 'noopener noreferrer',
+        'aria-label': label,
+      },
+      icon(styles.socialIcon),
+    ),
+  );
+}
+
+function createContactItem({ label, href, target, rel, icon }) {
+  const content = [icon(styles.contactIcon), label];
+
+  if (href) {
+    return createElement(
+      'li',
+      { className: styles.contactItem },
+      createElement(
+        'a',
+        {
+          className: styles.contactLink,
+          href,
+          ...(target && { target }),
+          ...(rel && { rel }),
+        },
+        ...content,
+      ),
+    );
+  }
+
+  return createElement(
+    'li',
+    { className: styles.contactItem },
+    createElement('span', { className: styles.contactText }, ...content),
+  );
+}
+
+export class Footer {
+  constructor() {
+    const socialLinks = createElement(
+      'div',
+      { className: styles.socialLinks },
+      ...createSocialLinks(),
+    );
+
+    const contactItems = CONTACT_ITEMS.map(createContactItem);
+    const contactList = createElement('ul', { className: styles.contactList }, ...contactItems);
+
+    const footerRight = createElement(
+      'div',
+      {
+        className: styles.footerRight,
+        id: 'contact-us',
+      },
+      createElement('h2', { className: styles.contactTitle }, 'Contact us'),
+      contactList,
+    );
+
+    const content = createElement(
+      'div',
+      { className: styles.content },
+      createElement(
+        'div',
+        { className: styles.footerLeft },
+        createElement(
+          'p',
+          { className: styles.title },
+          'Sip, Savor, Smile. ',
+          createElement('span', { className: styles.titleAccent }, 'It’s coffee time!'),
+        ),
+        socialLinks,
+      ),
+      footerRight,
+    );
+
+    this.element = createElement('footer', { className: styles.footer }, content);
+  }
+}
